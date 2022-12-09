9cda4fae5d33246e07de9d3421fe0ebc634215f9
t-kabu <t.kabu.03@gmail.com>
2022-12-09 16:15:56 +0900

    feat: mantine導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,30 @@
+import { MantineProvider } from '@mantine/core'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
   return (
     <>
-      <Component {...pageProps} />
+      <Head>
+        <title>writing support app</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'dark',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </>
   )
 }
