b9baad7af043dd36d847c5dd4caef205491946a3
t-kabu <t.kabu.03@gmail.com>
2022-12-14 19:49:18 +0900

    feat: sentencesの場所を変更

--- src/hooks/useAiResults.ts
+++ src/hooks/useAiResults.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { postApi } from 'utils/apiClient'
 
 type AiResults = Array<{
@@ -6,39 +6,27 @@ type AiResults = Array<{
   suggestions: string[]
 }>
 
-export const useAiResults = (richText: string): AiResults => {
+export const useAiResults = (sentences: string[]): AiResults => {
   const [aiResults, setAiResults] = useState<AiResults>([])
-
-  const sentences = useMemo(
-    () =>
-      richText
-        .replaceAll('<p>', '')
-        .replaceAll('</p>', '')
-        .replaceAll('<br>', '')
-        .split('。')
-        .slice(0, -1),
-    [richText],
-  )
 
   useEffect(() => {
     for (const sentence of sentences) {
-      if (!aiResults.every(v => v.key !== sentence)) {
-        return
+      if (aiResults.every(v => v.key !== sentence)) {
+        console.log('fetch!' + sentence)
+        ;(async () => {
+          const res = await postApi('/v1/edits', {
+            model: 'text-davinci-edit-001',
+            input: sentence,
+            instruction: 'Fix mistakes',
+            temperature: 0.5,
+            n: 3,
+          })
+          setAiResults(v => [
+            ...v,
+            { key: sentence, suggestions: res.choices.map(v => v.text) },
+          ])
+        })()
       }
-
-      ;(async () => {
-        const res = await postApi('/v1/edits', {
-          model: 'text-davinci-edit-001',
-          input: sentence,
-          instruction: 'Fix mistakes',
-          temperature: 0.5,
-          n: 3,
-        })
-        setAiResults(v => [
-          ...v,
-          { key: sentence, suggestions: res.choices.map(v => v.text) },
-        ])
-      })()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sentences])
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,80 @@
-import { Box, Flex, Text } from '@mantine/core'
-import { useDebouncedState } from '@mantine/hooks'
+import { Accordion, Box, Flex, List, Text } from '@mantine/core'
+import { useMemo, useState } from 'react'
 import { useAiResults } from 'hooks/useAiResults'
 import RichTextEditor from 'libs/RichTextEditor'
 
 const content = '<p>こんにちは</p>'
 
 export default function Home() {
-  // const editorRef = useRef<Editor>(null)
-  const [value, setValue] = useDebouncedState(content, 400)
-  const aiResults = useAiResults(value)
+  const [value, setValue] = useState(content)
+  const sentences = useMemo(
+    () =>
+      value
+        .replaceAll('<p>', '')
+        .replaceAll('</p>', '')
+        .replaceAll('<br>', '')
+        .split('。')
+        .slice(0, -1),
+    [value],
+  )
+  const aiResults = useAiResults(sentences)
 
   return (
     <main>
+      {/* <br />
+      {JSON.stringify(sentences)}
       <br />
-      {JSON.stringify(value)}
       <br />
       <br />
-      <br />
-      {JSON.stringify(aiResults)}
-      <Flex gap={15} m={100}>
-        <Box mt={6.5} sx={{ height: 700, overflow: 'hidden' }}>
-          {[...Array(300)].map((_, index) => (
-            <Text key={index} align="right" c={'gray.6'}>
-              {index + 1}
-            </Text>
-          ))}
-        </Box>
-        <Box
-          // ref={ref}
-          sx={{
-            width: 400,
-            wordWrap: 'break-word',
-            p: { margin: '0 !important', fontSize: 16, lineHeight: 1.55 },
-          }}
-        >
-          <RichTextEditor
-            // ref={editorRef}
-            value={value}
-            onChange={setValue}
+      {JSON.stringify(aiResults)} */}
+      <Flex gap="xl" mt="xl" mx="auto" w="85%">
+        <Flex gap="md">
+          <Box mt={6.5} sx={{ height: 700, overflow: 'hidden' }}>
+            {[...Array(300)].map((_, index) => (
+              <Text key={index} align="right" c={'gray.6'}>
+                {index + 1}
+              </Text>
+            ))}
+          </Box>
+          <Box
+            // ref={ref}
             sx={{
-              minHeight: 500,
-              height: '100%',
+              width: 400,
+              wordWrap: 'break-word',
+              p: { margin: '0 !important', fontSize: 16, lineHeight: 1.55 },
             }}
-            styles={{ toolbar: { padding: 0 } }}
-            controls={[]}
-          />
+          >
+            <RichTextEditor
+              value={value}
+              onChange={setValue}
+              sx={{
+                minHeight: 500,
+                height: '100%',
+              }}
+              styles={{ toolbar: { padding: 0 } }}
+              controls={[]}
+            />
+          </Box>
+        </Flex>
+        <Box w="100%">
+          <Accordion variant="separated" multiple>
+            {aiResults.map(result => {
+              if (sentences.includes(result.key)) {
+                return (
+                  <Accordion.Item key={result.key} value={result.key}>
+                    <Accordion.Control>{result.key}</Accordion.Control>
+                    <Accordion.Panel>
+                      <List type="ordered">
+                        {result.suggestions.map((str, index) => (
+                          <List.Item key={index}>{str}</List.Item>
+                        ))}
+                      </List>
+                    </Accordion.Panel>
+                  </Accordion.Item>
+                )
+              }
+            })}
+          </Accordion>
         </Box>
       </Flex>
     </main>
