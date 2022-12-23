758a4f213d6e907492d85ad575a6f1177af41169
t-kabu <t.kabu.03@gmail.com>
2022-12-23 12:18:00 +0900

    feat: replace機能実装

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -1,13 +1,78 @@
-import { Box, Accordion, Flex, CloseButton, List, Text } from '@mantine/core'
-import { FC } from 'react'
+import {
+  Box,
+  Accordion,
+  Flex,
+  CloseButton,
+  List,
+  Text,
+  Popover,
+  ActionIcon,
+  Button,
+  Group,
+} from '@mantine/core'
+import { IconReplace } from '@tabler/icons'
+import { FC, useState } from 'react'
 import { AiResults } from 'types/common'
+
+type ItemProps = {
+  sentence: string
+  onReplace: (sentence: string) => void
+}
+
+const Item: FC<ItemProps> = ({ sentence, onReplace }) => {
+  const [opened, setOpened] = useState(false)
+  return (
+    <Flex>
+      <Text fz="sm">{sentence}</Text>
+
+      <Popover
+        width={200}
+        position="bottom"
+        withArrow
+        shadow="md"
+        opened={opened}
+        onChange={setOpened}
+      >
+        <Popover.Target>
+          <ActionIcon variant="transparent" onClick={() => setOpened(v => !v)}>
+            <IconReplace size={18} />
+          </ActionIcon>
+        </Popover.Target>
+
+        <Popover.Dropdown>
+          <Group position="center">
+            <Button
+              size="xs"
+              variant="filled"
+              onClick={() => onReplace(sentence)}
+            >
+              Replace
+            </Button>
+            <Button
+              size="xs"
+              variant="outline"
+              onClick={() => setOpened(false)}
+            >
+              Cancel
+            </Button>
+          </Group>
+        </Popover.Dropdown>
+      </Popover>
+    </Flex>
+  )
+}
 
 type SuggestionsProps = {
   aiResults: AiResults
   sentences: string[]
+  onReplaceSentence: (from: string, to: string) => void
 }
 
-export const Suggestions: FC<SuggestionsProps> = ({ aiResults, sentences }) => {
+export const Suggestions: FC<SuggestionsProps> = ({
+  aiResults,
+  sentences,
+  onReplaceSentence,
+}) => {
   return (
     <Box w="100%">
       <Accordion
@@ -24,11 +89,15 @@ export const Suggestions: FC<SuggestionsProps> = ({ aiResults, sentences }) => {
                   <Accordion.Control>{result.key}</Accordion.Control>
                   <CloseButton />
                 </Flex>
+
                 <Accordion.Panel>
                   <List type="ordered">
                     {result.suggestions.map((str, index) => (
                       <List.Item key={index} p="xs">
-                        <Text fz="sm">{str}</Text>
+                        <Item
+                          sentence={str}
+                          onReplace={to => onReplaceSentence(result.key, to)}
+                        />
                       </List.Item>
                     ))}
                   </List>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Badge, Paper, SimpleGrid, Stack, Tabs } from '@mantine/core'
-import { IconSettings, IconSearch, IconBulb } from '@tabler/icons'
+import { Paper, SimpleGrid, Stack } from '@mantine/core'
 import { useMemo, useState } from 'react'
 import { AppLayout } from 'components/AppLayout'
 import { Editor } from 'components/Editor'
 import { Suggestions } from 'components/Suggestions'
+import { TabList } from 'components/TabList'
 import { useAiResults } from 'hooks/useAiResults'
 
 const content = '<p>テキスト</p>'
@@ -32,34 +32,26 @@ export default function Home() {
         </Paper>
 
         <Stack>
-          <Tabs value={activeTab} onTabChange={setActiveTab}>
-            <Tabs.List>
-              <Tabs.Tab
-                icon={<IconBulb size={14} />}
-                value="suggestions"
-                rightSection={
-                  <Badge
-                    sx={{ width: 16, height: 16, pointerEvents: 'none' }}
-                    variant="filled"
-                    size="xs"
-                    p={0}
-                  >
-                    {aiResults.map(v => sentences.includes(v.key) && 1).length}
-                  </Badge>
-                }
-              >
-                Suggestions
-              </Tabs.Tab>
-              <Tabs.Tab icon={<IconSearch size={14} />} value="search">
-                Search
-              </Tabs.Tab>
-              <Tabs.Tab icon={<IconSettings size={14} />} value="settings">
-                Settings
-              </Tabs.Tab>
-            </Tabs.List>
-          </Tabs>
+          <TabList
+            suggestionsCount={
+              aiResults.filter(v => sentences.includes(v.key)).length
+            }
+            value={activeTab}
+            onTabChange={setActiveTab}
+          />
+
           <Paper radius={'md'} shadow="sm" p="md" withBorder h={'100%'}>
-            <Suggestions aiResults={aiResults} sentences={sentences} />
+            {activeTab === 'suggestions' && (
+              <Suggestions
+                aiResults={aiResults}
+                sentences={sentences}
+                onReplaceSentence={(from, to) =>
+                  setValue(value.replace(from, to))
+                }
+              />
+            )}
+            {activeTab === 'search' && <>search</>}
+            {activeTab === 'settings' && <>settings</>}
           </Paper>
         </Stack>
       </SimpleGrid>
