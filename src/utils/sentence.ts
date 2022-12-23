a2abc31b12406e14380621372cd04cdbeaad187c
t-kabu <t.kabu.03@gmail.com>
2022-12-23 14:34:16 +0900

    feat: 文章周りのutils関数作成

---
+++ src/utils/sentence.ts
@@ -0,0 +1,20 @@
+// 例
+// <p>こんにちは。</p><p>私の名前は太郎です。</p><p><br/></p><p>今日は</p>
+// ↓
+// ["こんにちは。","私の名前は太郎です。"]
+export const toSentences = (richText: string): string[] => {
+  if (typeof window !== 'object') {
+    return []
+  }
+
+  const div = window.document.createElement('div')
+  div.innerHTML = richText
+  return div.innerText
+    .split('。')
+    .slice(0, -1)
+    .map(v => v + '。')
+}
+
+// 不自然なスペースと「。」を取り除き、「。」で終わる文章にする。
+export const sanitizeSentence = (sentence: string): string =>
+  sentence.trim().replaceAll('。', '') + '。'
