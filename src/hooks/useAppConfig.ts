95890e739538526a555413f7d502736069f6ae33
t-kabu <t.kabu.03@gmail.com>
2022-12-29 11:40:31 +0900

    feat: アプリの設定をローカルストレージに保存

--- src/hooks/useAppConfig.ts
+++ src/hooks/useAppConfig.ts
@@ -1,4 +1,4 @@
-import { atom, useAtom } from 'jotai'
+import { useLocalStorage } from '@mantine/hooks'
 
 export type AppConfig = {
   suggestionDirection:
@@ -10,10 +10,12 @@ export type AppConfig = {
   apiKey: string | undefined
 }
 
-const configAtom = atom<AppConfig>({
-  suggestionDirection: 'Make it a natural sentence.',
-  suggestionSensitivity: 60,
-  apiKey: undefined,
-})
-
-export const useAppConfig = () => useAtom(configAtom)
+export const useAppConfig = () =>
+  useLocalStorage<AppConfig>({
+    key: 'app-config',
+    defaultValue: {
+      suggestionDirection: 'Make it a natural sentence.',
+      suggestionSensitivity: 60,
+      apiKey: undefined,
+    },
+  })
